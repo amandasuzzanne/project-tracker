115e9009be0d88340a42950fbf97e70b661ae1a1
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-08 13:19:43 +0300

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/projects');
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error('Error fetching projects:', error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <div>
+      <h1>Projects</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Institution</th>
+            <th>Implementation Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <tr key={project.id}>
+              <td>{project.name}</td>
+              <td>{project.institution}</td>
+              <td>{project.implementation_date}</td>
+              <td>{project.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Home;
